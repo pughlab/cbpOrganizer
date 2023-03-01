241420c9bc4fc96fda9e99dc8842fbb55fee3e04
Mickey Ng <mickey.ng@rmp.uhn.ca>
2023-03-01 17:40:11 -0500

    add compress library and ability to extract uploaded tar file

--- cbporganizer-backend/src/main/java/cbporganizer/service/FilesStorageServiceImpl.java
+++ cbporganizer-backend/src/main/java/cbporganizer/service/FilesStorageServiceImpl.java
@@ -1,13 +1,19 @@
 package cbporganizer.service;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 
 @Service
 public class FilesStorageServiceImpl implements FilesStorageService{
@@ -25,12 +31,38 @@ public class FilesStorageServiceImpl implements FilesStorageService{
 
     @Override
     public void save(MultipartFile file) {
-        if (file != null) {
-            try {
-                Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                e.printStackTrace();
+        Path filePath = this.root.resolve(file.getOriginalFilename());
+        try {
+            Files.copy(file.getInputStream(), filePath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+
+            extractGZip(filePath.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void extractGZip(File inputFile) {
+        try {
+            TarArchiveInputStream tarInput = new TarArchiveInputStream(new GZIPInputStream(new FileInputStream(inputFile)));
+
+            TarArchiveEntry entry;
+            while((entry = tarInput.getNextTarEntry()) != null) {
+                if (entry.isDirectory()) {
+                    new File(root.resolve(entry.getName()).toString()).mkdirs();
+                } else {
+                    byte[] buffer = new byte[1024];
+                    File outFile = root.resolve(entry.getName()).toFile();
+                    outFile.getParentFile().mkdirs();
+                    try (FileOutputStream fos = new FileOutputStream(outFile)) {
+                        int len;
+                        while ((len = tarInput.read(buffer)) > 0) {
+                            fos.write(buffer, 0, len);
+                        }
+                    }
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
