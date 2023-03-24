e154faf2bc26b5a8f9311e4202060c5340a2ae2c
Mickey Ng <mickey.ng@rmp.uhn.ca>
2023-03-24 11:00:29 -0400

    code cleanup

--- cbporganizer-frontend/src/Component/ValidateComponent.tsx
+++ cbporganizer-frontend/src/Component/ValidateComponent.tsx
@@ -11,10 +11,8 @@ const ValidateComponent = () => {
 
     // validation message
     const [validateMessage, setValidateMessage] = useState('');
-    // show the html result button
-    // const [validateResponse, setValidateResponse] = useState<any>();
 
-    // test
+    // setting the shared context for the result component
     const { validationResult, setValidationResult } = useContext(SharedDataContext);
 
     useEffect(() => {
@@ -25,19 +23,12 @@ const ValidateComponent = () => {
     }, [response]);
 
     useEffect(() => {
-        console.log('responseReport: ', responseReport);
         if (responseReport !== null) {
             const file = new Blob([responseReport], {type: 'text/html'});
             const blobURL = URL.createObjectURL(file);
             setValidationResult(blobURL);
         }
     }, [responseReport]);
-
-    // useEffect(() => {
-    //     if (responseURL !== null) {
-    //         validateResponse(responseURL);
-    //     }
-    // }, [responseURL]);
 
     // check if there's any error
     useEffect(() => {
@@ -66,44 +57,16 @@ const ValidateComponent = () => {
         })
     }
 
-    // const handleShowURL = () => {
-    //     const file = new Blob([validateResponse], {type: 'text/html'});
-    //     const fileURL = URL.createObjectURL(file);
-    //     window.open(fileURL);
-    // }
-
-    // const handleShowReport = () => {
-    //     operationReport({
-    //         method: 'GET',
-    //         url: 'validation-report-html',
-    //         headers: {
-    //             'Content-type': 'application/json; charset=UTF-8',
-    //         },
-    //     })
-    // }
-
     return (
       <>
           <p>Validate your files with the dataset validator </p>
           <button onClick={handleValidate}>validate</button>
-          <button onClick={retreieveReportAsBlob}>set validation</button>
-          {/*<button onClick={handleShowReport}>show report inline</button>*/}
           {loading && <div className="card flex justify-content-center">
               <ProgressSpinner />
           </div>}
           <br/>
           <br/>
           {validateMessage && <p>{validateMessage}</p>}
-          {/*<br />*/}
-          {/*{validateResponse && (*/}
-          {/*    <div>*/}
-          {/*        <button onClick={handleShowURL}>Open Result</button>*/}
-          {/*    </div>*/}
-          {/*)}*/}
-          {/*{htmlBlob && (*/}
-          {/*    <iframe src={htmlBlob} title="HTML Content" width="100%"*/}
-          {/*            height="70%" />*/}
-          {/*)}*/}
       </>
     );
 }
